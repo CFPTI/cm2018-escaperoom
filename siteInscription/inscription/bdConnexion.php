b21beb2f0e404dab7cdae72528ba6edeff63a792
LAAC172003 <61194486+LAAC172003@users.noreply.github.com>
2021-09-15 10:29:22 +0200

    bdConnexion
    Liaison a la base de données

---
+++ siteInscription/inscription/bdConnexion.php
@@ -0,0 +1,30 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_NAME = '';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+
+function getConnexion()
+{
+    static $db = NULL;
+    try {
+        if ($db == NULL) {
+            $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD, array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ));
+        }
+    } catch (PDOException $e) {
+        echo "DB connection error, see logs.";
+        var_dump($e);
+        error_log($e->getMessage());
+    }
+    return $db;
+}
+
+$conn = getConnexion();
+
+?>
