edd50426990c12a7671a0626d9ccd01ed5e94b77
laelgary <lael.umtsn@eduge.ch>
2021-09-27 08:02:12 +0200

    ajouter horloge

--- siteInscriptionV2/index.php
+++ siteInscriptionV2/index.php
@@ -1,3 +1,6 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +11,49 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <header></header>
-    <main></main>
+    <form method="post">
+        <header>
+            <h1>SESSION ESCAPE GAME</h1>
+            <p>Nombre de personne: <input type="number" ></p>
+        </header>
+        <main>
+            <div id="MyClockDisplay" class="clock" onload="showTime()"></div>
+            <input type="submit" class="lancement" value="Commencer la session">
+            <article>
+                
+            </article>
+        </main>
+    </form>
+    <script>
+        function showTime(){
+    let date = new Date();
+    let h = date.getHours(); // 0 - 23
+    let m = date.getMinutes(); // 0 - 59
+    let s = date.getSeconds(); // 0 - 59
+    let session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    let time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+    }
+
+    showTime();
+    </script>
 </body>
 </html>
